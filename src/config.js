262924929ff62713eb700de8e8bb552921593bf0
Freddie Ridell <freddie@codogo.io>
2018-01-04 14:21:22 +0000

    feat(config-and-install): config getter and service installer

--- src/config.js
+++ src/config.js
@@ -1,3 +1,70 @@
-module.exports = {
-	dataFolder: `${require("os").homedir()}/Sync/Files/Jask`,
-};
+import fs from "fs";
+import R from "ramda";
+
+const dotfilePath = `${require("os").homedir()}/.jaskrc.js`;
+let config = null;
+
+export default () =>
+	config
+		? Promise.resolve(config)
+		: new Promise(done => {
+			try {
+				config = require(dotfilePath);
+				done(config);
+			} catch (e) {
+				if (e.code === "MODULE_NOT_FOUND") {
+					const possible =
+							"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+					const generateKey = () =>
+						R.times(
+							() =>
+								possible.charAt(
+									Math.floor(
+										Math.random() * possible.length,
+									),
+								),
+							128,
+						).join("");
+
+					fs.writeFile(
+						dotfilePath,
+						`module.exports = {
+	dataFolder: \`${require("os").homedir()}/.jaskActions\`,
+	server: {
+		port: 9000,
+		key: "${generateKey()}",
+	},
+	client: {
+		userHTTPServer: false,
+		rendering: {
+			//these will eventually be depricated, in favour of storing such things in the action store.
+			//this will allow for universal cross clien config
+			giveScore: ({ uuid, due, created, updated, done, tags, project, prioirty, }, { now, }) => {
+				return Math.pow(10, ( ( due - now ) / 4320000 ) + 1);
+			},
+			filterAction: ({ done, }) => !!done,
+			headers: [
+				"score",
+				"id",
+				"description",
+				"due",
+				"tags",
+				"priority",
+				"project",
+				"depends",
+				"recur",
+			],
+		}
+	},
+}`,
+						() => {
+							console.log(
+								`created a config file at ${dotfilePath}`,
+							);
+							config = require(dotfilePath);
+							done(config);
+						},
+					);
+				}
+			}
+		});
--- src/index.js
+++ src/index.js
@@ -5,92 +5,63 @@ import recursive from "recursive-readdir";
 import R from "ramda";
 import url from "url";
 
-const dotfilePath = `${require("os").homedir()}/.jaskServerConfig.js`;
-let config = {};
+import getConfig from "./config";
 
-const getAllEventsIdents = () =>
-	new Promise(done => {
-		recursive(config.dataFolder, (err, files) => {
-			done(files || []);
-		});
-	})
-		.then(files =>
-			Promise.all(
-				R.pipe(
-					R.sortBy(
-						R.pipe(R.replace(/.+\//, ""), x => parseInt(x, 10)),
-					),
-					R.map(R.replace(config.dataFolder + "/", "")),
-				)(files),
-			),
-		)
-		.then(x => JSON.stringify(x));
-
-const getEvent = path =>
-	new Promise(done =>
-		jsonfile.readFile(config.dataFolder + path, (err, dat) => done(dat)),
-	).then(x => JSON.stringify(x));
-
-const startServer = () => {
-	http
-		.createServer((req, res) => {
-			const parsedUrl = url.parse(req.url);
-
-			if (req.headers.authorization !== config.key) {
-				res.statusCode = 401;
-				return res.end(
-					'{err:401, msg: "not authorised, please provide key"}',
-				);
-			}
-
-			if (parsedUrl.pathname === "/") {
-				res.setHeader("Content-type", "application/json");
-				getAllEventsIdents().then(data => res.end(data));
-			} else {
-				res.setHeader("Content-type", "application/json");
-				getEvent(parsedUrl.pathname).then(data => res.end(data));
-			}
+getConfig().then(config => {
+	const { dataFolder, server: { port, key, }, } = config;
+	const getAllEventsIdents = () =>
+		new Promise(done => {
+			recursive(dataFolder, (err, files) => {
+				done(files || []);
+			});
 		})
-		.listen(config.port);
+			.then(files =>
+				Promise.all(
+					R.pipe(
+						R.sortBy(
+							R.pipe(R.replace(/.+\//, ""), x => parseInt(x, 10)),
+						),
+						R.map(R.replace(dataFolder + "/", "")),
+					)(files),
+				),
+			)
+			.then(x => JSON.stringify(x));
 
-	console.log(`
+	const getEvent = path =>
+		new Promise(done =>
+			jsonfile.readFile(dataFolder + path, (err, dat) => done(dat)),
+		).then(x => JSON.stringify(x));
+
+	const startServer = () => {
+		http
+			.createServer((req, res) => {
+				const parsedUrl = url.parse(req.url);
+
+				if (req.headers.authorization !== key) {
+					res.statusCode = 401;
+					return res.end(
+						'{err:401, msg: "not authorised, please provide key"}',
+					);
+				}
+
+				if (parsedUrl.pathname === "/") {
+					res.setHeader("Content-type", "application/json");
+					getAllEventsIdents().then(data => res.end(data));
+				} else {
+					res.setHeader("Content-type", "application/json");
+					getEvent(parsedUrl.pathname).then(data => res.end(data));
+				}
+			})
+			.listen(port);
+
+		console.log(`
 Server started
 ==============
-	+ on port ${config.port}
-	+ serving from ${config.dataFolder}
-	+ with auth key: "${config.key}"
+	+ on port ${port}
+	+ serving from ${dataFolder}
+	+ with auth key: "${key}"
 `);
-};
+	};
 
-console.log(`looking for config file at ${dotfilePath}`);
-try {
-	config = require(dotfilePath);
 	startServer();
-} catch (e) {
-	if (e.code === "MODULE_NOT_FOUND") {
-		const possible =
-			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		const generateKey = () =>
-			R.times(
-				() =>
-					possible.charAt(
-						Math.floor(Math.random() * possible.length),
-					),
-				128,
-			).join("");
-
-		fs.writeFile(
-			dotfilePath,
-			`module.exports = {
-	dataFolder: \`${require("os").homedir()}/.jaskActions\`,
-	port: 9000,
-	key: "${generateKey()}",
-}`,
-			() => {
-				console.log(`created a config file at ${dotfilePath}`);
-				config = require(dotfilePath);
-				startServer();
-			},
-		);
-	}
-}
+});
